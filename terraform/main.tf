b14336d5e5566b367eb62e9d62749ac0ebca15de
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2019-10-08 21:04:35 -0500

    very barebones SNS topic

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+
+
+resource "aws_sns_topic" "test" {
+  name = "my-topic-with-policy"
+  delivery_policy = <<EOF
+{
+  "http": {
+    "defaultHealthyRetryPolicy": {
+      "minDelayTarget": 20,
+      "maxDelayTarget": 20,
+      "numRetries": 3,
+      "numMaxDelayRetries": 0,
+      "numNoDelayRetries": 0,
+      "numMinDelayRetries": 0,
+      "backoffFunction": "linear"
+    },
+    "disableSubscriptionOverrides": false,
+    "defaultThrottlePolicy": {
+      "maxReceivesPerSecond": 1
+    }
+  }
+}
+EOF
+}
