a5493c267907fed373f23f284ab14ff843cf9614
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2019-10-09 18:53:30 -0500

    access policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,3 +22,40 @@ resource "aws_sns_topic" "test" {
 }
 EOF
 }
+
+resource "aws_sns_topic_policy" "default" {
+  arn = "${aws_sns_topic.test.arn}"
+
+  policy = "${data.aws_iam_policy_document.sns-topic-policy.json}"
+}
+
+data "aws_iam_policy_document" "sns-topic-policy" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      "${aws_sns_topic.test.arn}",
+    ]
+
+    sid = "__default_statement_ID"
+  }
+}
